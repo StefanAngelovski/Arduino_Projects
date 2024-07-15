f52202f6f0e7ac5b0a49a8324edc5ad8ed9e6635
StefanAngelovski <stefanangelovski2002@gmail.com>
2024-07-15 09:56:25 +0200

    Update BluetoothSpeaker.ino

--- BluetoothSpeaker/BluetoothSpeaker.ino
+++ BluetoothSpeaker/BluetoothSpeaker.ino
@@ -1,9 +1,9 @@
 #include "esp32_bt_music_receiver.h"
 
-BluetoothA2DSink a2dp_sink; //initialize bluetooth speaker library
+BluetoothA2DSink a2dp_sink; //Initialize bluetooth speaker library
 
 void setup() {
-    a2dp_sink.start("Zvucnik na Stefan"); //start bluetooth with the name in ""
+    a2dp_sink.start("My Bluetooth Speaker"); //Start bluetooth with the name in ""
 }
 
 void loop() {
